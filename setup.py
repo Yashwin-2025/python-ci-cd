5e7cb3d061a3073c9cbdb0f53c8a8983ada4a056
YASHWIN S <yashwins@YASHWINs-MacBook-Air.local>
2025-03-11 11:51:05 +0530

    Switch to pyproject.toml for package management

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name="python-ci-cd",  # Rename package here
-    version="1.0.0",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    description="A simple Python package for CI/CD testing.",
-    author="Your Name",
-)
